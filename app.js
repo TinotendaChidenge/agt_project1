dc3ecd9e22223f5cf2d4e6764f3d5b512262c82b
TinotendaChidenge <142484728+TinotendaChidenge@users.noreply.github.com>
2024-04-23 16:34:18 +0200

    second commit

--- app.js
+++ app.js
@@ -8,6 +8,22 @@ app.post("/tino",(req, res) => {
  res.send("hello world!")
 }); 
 
+app.get("/tino",(req, res) => {
+    res.send("hello tino!")
+   }); 
+
+app.put("/tino",(req, res) => {
+    res.send("hello siya!")
+   }); 
+
+app.patch("/tino",(req, res) => {
+    res.send("hello blessing!")
+   }); 
+
+app.delete("/tino",(req, res) => {
+    res.send("hello ano!")
+   }); 
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
